13271681384127238d1e5f3bf781cfba68f38238
Tyler Ochiai <tyler.a.ochiai@gmail.com>
2014-04-24 17:11:41 -0700

    Add showAlert() to main.js

--- js/main.js
+++ js/main.js
@@ -14,8 +14,20 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new LocalStorageStore();
+        var self = this;
+        this.store = new LocalStorageStore(function() {
+            self.showAlert('Store Initialized', 'Info');
+        });
+        
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+
+    showAlert: function(message, title) {
+        if(navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
     }
 
 };
